aac1764915ab33c338f32d91cbf87d069c3693b5
yj007cho <yj007cho8@gmail.com>
2026-08-13 16:26:25 +0900

    03 up4

---
+++ yolo_test1.py
@@ -0,0 +1,42 @@
+from ultralytics import YOLO
+import cv2
+
+
+model = YOLO("src/models/YOLO/yolo11n.pt")
+model.to("cuda")
+
+pipeline = (
+    "nvarguscamerasrc sensor-id=0 ! "
+    "video/x-raw(memory:NVMM), width=1280, height=720, framerate=30/1 ! "
+    "nvvidconv ! "
+    "video/x-raw, format=BGRx ! "
+    "videoconvert ! "
+    "video/x-raw, format=BGR ! "
+    "queue leaky=downstream max-size-buffers=1 ! "
+    "appsink drop=true max-buffers=1 sync=false"
+)
+
+cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+    results = model.predict(
+        source=frame,   # source image
+        conf=0.25,      # Confidence Threshold
+        iou=0.5,        # IoU Threshold
+        verbose=False,  # no output prints
+        classes=None,   # selected class
+    )
+
+    output_frame = results[0].plot()
+
+    cv2.imshow("YOLO Object Detection", output_frame)
+
+cap.release()
+cv2.destroyAllWindows()
